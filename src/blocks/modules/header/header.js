c2d80cdcdd27bf0b6f92fee86d97e1af7d29f258
S19S93 <S19S93@yandex.ru>
2021-11-11 18:32:10 +0200

    Изменения логотипа при прокрутки

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,12 +1,15 @@
 (function fixedMenuScroll() {
     const header = document.querySelector('.header');
+    const logo = document.querySelector('.logo-header');
 
-    if (header) {
+    if (header && logo) {
         window.addEventListener('scroll', function () {
             if (window.scrollY > 0) {
                 header.classList.add('header--fixed');
+                logo.classList.add('logo-orange');
             } else {
                 header.classList.remove('header--fixed');
+                logo.classList.remove('logo-orange');
             }
         })
     }
