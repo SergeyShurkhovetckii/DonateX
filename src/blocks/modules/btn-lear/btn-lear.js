425194a014e68e275c90b855d068be3e74789144
S19S93 <S19S93@yandex.ru>
2021-11-11 17:28:35 +0200

    about

---
+++ src/blocks/modules/btn-lear/btn-lear.js
@@ -0,0 +1,13 @@
+modules.define('btn-lear', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
