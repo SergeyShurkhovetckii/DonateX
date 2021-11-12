8903de9a552acb02e410446278125f8ee4278a7c
S19S93 <S19S93@yandex.ru>
2021-11-12 12:30:43 +0200

    Swiper

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -5,6 +5,8 @@ import "%modules%/header-burger/header-burger";
 
 // Мобильное меню 
 import "%modules%/m-menu/m-menu";
-// 
+
+// Слайдер
+import "%modules%/slider/slider";
 
 import "%modules%/footer/footer"
